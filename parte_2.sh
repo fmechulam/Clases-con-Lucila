c90375a298895b6c2704708225fe0ef7066cf1b8
fmechulam <fmechulam@frba.utn.edu.ar>
2025-04-02 18:14:27 +0000

    terminada parte 2, falta probar

--- parte_2.sh
+++ parte_2.sh
@@ -30,8 +30,26 @@ while IFS= read NOMBRE_POKEMON && [-n "$NOMBRE_POKEMON"]; do
         continue
     fi
 
-#obtenemos altura y peso
-ALTURA=$(grep -i ",$NOMBRE_POKEMON" "ARCHIVO_POKEMON" | cut -d',' -f4)
+    #obtenemos altura y peso
+    ALTURA=$(grep -i ",$NOMBRE_POKEMON," "ARCHIVO_POKEMON" | cut -d',' -f4)
+    PESO=$(grep -i ",$NOMBRE_POKEMON," "ARCHIVO_POKEMON" | cut -d',' -f5)
+
+    echo "-------------------------"
+    echo "pokemon $NOMBRE_POKEMON"
+    echo "altura : $ALTURA"
+    echo "peso : $PESO"
+    echo "habilidades :"
+
+    #obtenemos las habilidades
+    HABILIDADES=$(grep "^ID," "$ARCHIVO_HABILIDADES" | cut -d',' -f2)
+
+    for $HABILIDAD_ID in $HABILIDADES; do
+        HABILIDAD_NOMBRE=$(grep "^HABILIDAD_ID,7," "$ARCHIVO_NOMBRES_HABILIDADES" | cut -d',' -f3)
+        if [ -n $HABILIDAD_NOMBRE ] 
+            echo " * $HABILIDAD_NOMBRE"
+        fi
+    done
+done
 
 
 
